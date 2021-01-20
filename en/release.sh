c4718579f61429012b0d994b04796f2d4981c703
chautran <chau.tran@bossrotorcompany.com>
2021-01-20 16:00:32 +0700

    update

---
+++ en/release.sh
@@ -0,0 +1,16 @@
+bundle install
+bundle exec jekyll build
+python3 remove_space_href.py
+cp CNAME _site/
+
+git add .
+git commit -m 'release'
+git push bb main
+
+cd _site
+git init
+git remote add origin https://github.com/dev-yakuza/dev-yakuza.github.io.git
+git add .
+git commit -m 'release'
+git fetch
+git push origin master -f
